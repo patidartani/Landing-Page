d972ea57157aa0905859b002a31738367574ec9f
patidartani <tanishapatidar905@gmail.com>
2023-10-29 14:16:47 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,201 @@
+import React from 'react'
+import "./Home.css"
+
+const Home = () => {
+  return (
+<div className='Home'>
+<div className="main">
+          <div className="content">
+                 <div className="top">
+                    <div className="left">
+                              <p>LOGO</p>
+                    </div>
+                    <div className="medium">
+                              <div className="mid">
+                              <div className="le">
+                              <i class="ri-search-line"></i>
+                               <small>Search here...</small>
+                              </div>
+                              <div className="ri">
+                              <i class="ri-equalizer-line"></i>
+                               <span>Filters</span>
+                              </div>
+                              </div>
+                    </div>
+                    <div className="right">
+                              <p> Become a Seller</p>
+                    </div>
+                 </div>
+                 <div className="bottom">
+                 <div className="Left">
+                    <div className="Ltp">
+                    <p><i class="ri-home-5-line"></i><span>Home</span></p>     
+                    <p><i class="ri-notification-line"></i><span>Notifications</span></p>     
+                    <p><i class="ri-heart-3-line"></i><span>Shop</span></p>     
+                    <p><i class="ri-message-line"></i><span>Conversation</span></p>     
+                    <p><i class="ri-wallet-line"></i><span>Wallet</span></p>     
+                    <p><i class="ri-star-s-line"></i><span>Subscription</span></p>     
+                    <p><i class="ri-user-3-line"></i><span>My Profile</span></p>     
+                    <p><i class="ri-settings-4-line"></i><span>Settings</span></p>      
+                    </div>
+                    <div className="Lbt">
+                          <p><i class="ri-logout-circle-line"></i><span>Log out</span></p>     
+                    </div>
+                 </div>
+                    <div className="Medium">
+                    <div className="MainContent">
+                            <div className="Box">
+                                <div className="Btop">
+                                     <div className="one">
+                                        <div className="Oneleft">
+                                            <img src="https://media.istockphoto.com/id/671294474/photo/attractive-girl-in-a-black-hat-worn-on-the-head-on-the-beach.webp?b=1&s=170667a&w=0&k=20&c=-nJh_dZ3h1qSAMmSqc8P9Nxn7ELN1zpia47iwIZw9Bk=" alt="" />
+                                            <div className="heading">
+                                            <span>Lara Leones</span> <small>@thewallart</small>
+                                            </div>
+                                        </div>
+                                        <div className="Oneright">
+                                        <i class="ri-more-2-fill"></i>   
+                                        </div>
+                                     </div>
+                                     <div className="two">
+                                        <p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<span>Read More</span></p>
+                                     </div>
+                                     <div className="three"></div>
+                                </div>
+                                <div className="Bbtm">
+                                   <div className="subscribe">
+                                         <div className="icon">
+                                         <i class="ri-heart-line"></i>
+                                        <span>9.8k</span>    
+                                         </div>
+                                         <div className="icon">
+                                         <i class="ri-chat-1-line"></i>
+                                                  <span>8.6k</span>
+                                         </div>
+                                         <div className="icon">
+                                         <i class="ri-share-forward-line"></i>
+                                                  <span>7.2k</span>
+                                         </div>
+                                   </div>
+                                </div>
+                            </div>
+                            <div className="Box">
+                            <div className="Btop">
+                                     <div className="one">
+                                        <div className="Oneleft">
+                                            <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR645xka-StTAonZ5MY8f4MtbjglVEgydpnw_jgAKHE2AbvT32E
+                                            
+                                            " alt="" />
+                                            <div className="heading">
+                                            <span>Thomas J.</span> <small>@thecustomcreater</small>
+                                            </div>
+                                        </div>
+                                        <div className="Oneright">
+                                        <i class="ri-more-2-fill"></i>   
+                                        </div>
+                                     </div>
+                                     <div className="two">
+                                        <p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<span>Read More</span></p>
+                                     </div>
+                                     <div className="four"></div>
+                                </div>
+                                <div className="Bbtm">
+                                   <div className="subscribe">
+                                         <div className="icon">
+                                         <i class="ri-heart-line"></i>
+                                        <span>9.8k</span>    
+                                         </div>
+                                         <div className="icon">
+                                         <i class="ri-chat-1-line"></i>
+                                                  <span>8.6k</span>
+                                         </div>
+                                         <div className="icon">
+                                         <i class="ri-share-forward-line"></i>
+                                                  <span>7.2k</span>
+                                         </div>
+                                   </div>
+                                </div>
+                              </div>
+                              <div className="BoxLast">
+                                       <div className="lastOne">
+                                         <img src="https://myindianthings.com/cdn/shop/products/ArtImageCorrection_c7d9709c-1414-413f-9501-31859bce9604_400x.jpg?v=1667889484" alt="" />
+                                         <p>Modern Wall Decor Framed Painting</p>
+                                         <div className="line">
+                                        <h6>$199.99</h6>   
+                                        <span>
+                                        <i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i>       
+                                        </span>    
+                                         </div>
+                                        </div> 
+                                       <div className="lastTwo">
+                                       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLD2URO4jGRTu1gzV3fxakOAapr8HlxXX5KIdoq8nODYkgPLjY" alt="" />
+                                         <p>Modern Wall Decor Framed Painting</p>
+                                         <div className="line">
+                                        <h6>$199.99</h6>   
+                                        <span>
+                                        <i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i>        
+                                        </span>    
+                                         </div>
+                                       </div>
+                              </div>
+                    </div>
+                    </div>
+                    <div className="Right">
+                    <div className="RightMain">
+                                 <h5 style={{marginTop:"0.5vmax", fontFamily:"monospace", fontWeight:"800"}}> Artists Photography</h5>
+                              <div className="RightImg1">
+                                <div className="text">
+                                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRqF6QjPZELXzza9xltGiPQ28if96_KTghAg&usqp=CAU" alt="" /> 
+                              <div className="name">
+                                        <h5>Thomas Edward</h5>
+                                        <p>@thewildwithyou</p>
+                              </div> 
+                                </div>
+                              </div>
+                              <div className="RightImg2">
+                              <div className="text">
+                                      <img src="https://www.format.com/wp-content/uploads/eddy-klaus-33079-unsplash.jpg" alt="" /> 
+                              <div className="name">
+                                        <h5>Chris Doe</h5>
+                                        <p>@thewildwithyou</p>
+                              </div> 
+                                </div>
+                              </div>
+                              <div className="RightImg3">
+                              <div className="text">
+                                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJE_TYT0I3lW3sAyaKZRU0y565TVnxwGTwFIFKX-okY7UI0m1AS7oYkiY_eqr-0Towrnc&usqp=CAU" alt="" /> 
+                              <div className="name">
+                                        <h5>Emilie Jones</h5>
+                                        <p>@thewildwithyou</p>
+                              </div> 
+                                </div>
+                              </div>
+                              <div className="RightImg4">
+                              <div className="text">
+                                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVLHmowDvlr5MFKSUxwt_SZfA2jzv80JI80A&usqp=CAU" alt="" /> 
+                              <div className="name">
+                                        <h5>Jessica Williams</h5>
+                                        <p>@thewildwithyou</p>
+                              </div> 
+                                </div>
+                              </div>
+                              <div className="RightImg5">
+                              <div className="text">
+                                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDSvxVwRPTfklldyquBoxMRJvYlIDjOJiFCUbp2u_y5E3fT3Kv4-j7jqMPHr3Vv2uYu8c&usqp=CAU" alt="" /> 
+                              <div className="name">
+                                        <h5>Thomas Edward</h5>
+                                        <p>@thewildwithyou</p>
+                              </div> 
+                                </div>
+                              </div>
+                              
+                    </div>
+                    </div>
+          </div>
+          </div>
+ </div>
+</div>
+  )
+}
+
+export default Home
